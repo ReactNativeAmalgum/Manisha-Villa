1b59a9261f1ac4fd1eb0cb166e316c4f94744d60
mukund-skdm1-developer <mukund@skdm.in>
2024-10-11 15:13:03 +0530

    banner improved

--- src/Inc/CarouselBanner.jsx
+++ src/Inc/CarouselBanner.jsx
@@ -3,13 +3,13 @@ import { Carousel } from "react-bootstrap";
 import "./banner.css";
 import manish1Img from "../Assets/Images/Manisha1/indexManish1";
 import { Fade, Slide } from "react-awesome-reveal";
-import Navbar from '../Components/Routes/Navbar';
-import vidoe from '../Assets/Images/banner-video.mp4'
+import Navbar from "../Components/Routes/Navbar";
+import vidoe from "../Assets/Images/banner-video.mp4";
 const CarouselBanner = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const settings = {
-    interval: 3000,
+    // interval: 000,
     pause: "hover",
     controls: true,
     indicators: true,
@@ -50,7 +50,7 @@ const CarouselBanner = () => {
 
   return (
     <div className="carousel-container">
-      <div style={{ position: 'absolute', width: '100%' }}>
+      <div style={{ position: "absolute", width: "100%" }}>
         <Navbar />
       </div>
 
@@ -64,24 +64,46 @@ const CarouselBanner = () => {
       >
         {slides.map((slide) => (
           <Carousel.Item key={slide.id}>
-            <video className="d-block w-100 slider-img" autoPlay loop muted>
-              <source src={vidoe} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+
 
             <div className="carousel-overlay" />
           </Carousel.Item>
         ))}
       </Carousel>
+      <video
+        // onLoadedMetadata={(e) => (e.target.playbackRate = 0.05)}
+        className="d-block w-100 slider-img"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={vidoe} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="text-box">
-
-        <Slide direction={getSlideDirection2(currentSlide)} duration={2000} delay={500}>
-          <span style={{ backgroundColor: "white", color: "black", padding: 10, fontWeight: 500 }} className="category">
+        <Slide
+          direction={getSlideDirection2(currentSlide)}
+          duration={2000}
+          delay={500}
+        >
+          <span
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              padding: 10,
+              fontWeight: 500,
+            }}
+            className="category"
+          >
             Manissha, <span style={{ color: "#f35525" }}>Bunglow</span>
           </span>
         </Slide>
 
-        <Slide direction={getSlideDirection(currentSlide)} duration={2000} delay={500}>
+        <Slide
+          direction={getSlideDirection(currentSlide)}
+          duration={2000}
+          delay={500}
+        >
           <h3 className="txt-h3">{slides[currentSlide].text}</h3>
         </Slide>
       </div>
